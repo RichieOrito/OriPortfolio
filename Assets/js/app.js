5f29d8859a73037b7dd602d6eda4e54d3422e5e4
RichieOrito <oritorichie2@gmail.com>
2022-06-10 03:18:38 +0300

    created a function for the button when clicked to be active 'only when clicked should it be active'

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -2,3 +2,19 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
+
+function pageTransition(){
+    //Button click active class
+    //get rid of a class that is not active
+    for(let i =0; i< sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            //select current btn
+            let currentBtn = document.querySelectorAll('.active-btn');
+            //if a btn is not selected(active-btn) let it be replaced with an empty string
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn'
+        })
+    }
+}
+
+pageTransition();
