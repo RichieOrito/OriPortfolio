4ba4d26367a540a96ad2bd84a0f0586b28ebbac0
RichieOrito <oritorichie2@gmail.com>
2022-06-14 03:21:18 +0300

    changes class active to active-btn

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -24,11 +24,11 @@ function pageTransition(){
             //remove selected from the other btns
             //looping through the container of the buttons for each individual btn.
             //if we are not clicking on a btn we get rid of the active class
-           sectBtns.forEach((btn) =>{
-               btn.classList.remove('active')
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active-btn')
            })
             //Adding active to whatever btn I have clicked.
-            e.target.classList.add('active')
+            e.target.classList.add('active-btn')
 
             //hide other sections
             //when clicking the active button I want set the display and transition back to default
