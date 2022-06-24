fa0e79352a6b5d1c2145b6d87cf1d5a29db50efd
RichieOrito <oritorichie2@gmail.com>
2022-06-24 05:37:55 +0300

    makes the toggle theme interactive

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -40,6 +40,12 @@ function pageTransition(){
             element.classList.add('active');
         }
     })
+    //Toggle them
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 
