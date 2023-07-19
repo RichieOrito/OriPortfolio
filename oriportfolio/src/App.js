f210db1b486b33aa8848ccef5923a106a200a3c9
Richard Orito <oritorichie2@gmail.com>
2023-07-19 20:25:02 +0300

    all file modified updated

--- oriportfolio/src/App.js
+++ oriportfolio/src/App.js
@@ -1,7 +1,30 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../src/pages/home'
+import About from '../src/pages/about';
+import Projects from '../src/pages/projects';
+import Myblogs from '../src/pages/myblogs';
+import Contact from '../src/pages/contact';
+import Sidebar from '../src/components/sidebar'
 
-function App() {
-  
-}
+import "./App.css";
+
+const App = () => {
+
+  const sectionIds = ['home', 'about', 'portfolio', 'blogs', 'contact'];
+
+  return (
+      <div>
+        <Home />
+        <Sidebar sectionIds={sectionIds} />
+        <About />
+        <Projects />
+        <Myblogs />
+        <Contact />
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
