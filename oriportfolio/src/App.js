c12e73e07833750a063289e15a57aafb244f1a7a
Richard Orito <oritorichie2@gmail.com>
2023-08-01 14:34:48 +0300

    updated the Helmet and the App layout to index

--- oriportfolio/src/App.js
+++ oriportfolio/src/App.js
@@ -1,34 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from '../src/pages/home'
-import About from '../src/pages/about';
-import Projects from '../src/pages/projects';
-import Myblogs from '../src/pages/myblogs';
-import Contact from '../src/pages/contact';
-import Sidebar from '../src/components/sidebar'
-
-import { BrowserRouter } from 'react-router-dom';
-
 import "./App.css";
 
-const App = () => {
+import Layout from "./components/layout/Layout";
 
-  const sectionIds = ['home', 'about', 'portfolio', 'blogs', 'contact'];
-
-  return (
-      <div>
-        <BrowserRouter>
-          <Home />
-          <Sidebar sectionIds={sectionIds} />
-          <About />
-          <Projects />
-          <Myblogs />
-          <Contact />
-        </BrowserRouter>
-      </div>
-  );
+function App() {
+  return <Layout />;
 };
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
--- oriportfolio/src/index.js
+++ oriportfolio/src/index.js
@@ -2,20 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { Helmet } from 'react-helmet';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Helmet>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500&display=swap" rel="stylesheet" />
-      <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerPolicy="no-referrer" />
-    </Helmet>
+    <BrowserRouter>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500&display=swap" rel="stylesheet" />
+        <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerPolicy="no-referrer" />
+      </Helmet>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
